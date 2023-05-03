ea7635f42faa316dd31052a5f7a64e5e4bccb169
CarloBattista <carlitobatti@gmail.com>
2023-05-03 14:00:09 +0200

    creazione componente header

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
 <script>
+import Header from "./components/Header.vue"
 
+export default {
+  name: "App",
+  components: {
+    Header
+  }
+}
 </script>
 
 <template>
-
+  <Header />
 </template>
 
-<style scoped>
-
-</style>
+<style lang="scss"></style>
---
+++ src/components/Header.vue
@@ -0,0 +1,11 @@
+<script>
+export default {
+    name: "Header",
+}
+</script>
+
+<template>
+    <div>
+        <h1>Header</h1>
+    </div>
+</template>
