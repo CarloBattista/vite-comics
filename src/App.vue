ed7d35e0f00f7a6d1c78056449e5e89a528dc691
CarloBattista <carlitobatti@gmail.com>
2023-05-03 14:36:18 +0200

    header

--- src/App.vue
+++ src/App.vue
@@ -15,6 +15,8 @@ export default {
 
 <style lang="scss">
 @use "./style/main.scss" as *;
+@import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital,wght@0,200;0,300;0,400;0,600;0,700;0,900;1,200;1,300;1,400;1,600;1,700;1,900&display=swap');
+
 * {
   box-sizing: border-box;
   margin: 0;
@@ -28,6 +30,7 @@ body {
   font-family: 'Source Sans Pro', sans-serif;
   background: #fff;
   color: #1C1C1C;
+  user-select: none;
 }
 
 </style>
