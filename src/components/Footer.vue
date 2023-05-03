142b67cddeaab4f2cb1909bf783f039aa897c124
CarloBattista <carlitobatti@gmail.com>
2023-05-03 15:26:38 +0200

    footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -7,6 +7,101 @@ export default {
 <template>
     <!-- Footer -->
     <div class="footer">
-        <h1>footer</h1>
+        <div class="inner_footer">
+            <!-- Link Footer -->
+            <div class="links_footer_row_container">
+                <ul class="row_links_footer">
+                    <div class="up_footer_links">
+                        <li class="headingLink_footer">Dc Comics</li>
+                        <li class="bodyLink_footer">
+                            <a href="/">Characters</a>
+                        </li>
+                        <li class="bodyLink_footer">
+                            <a href="/">Comics</a>
+                        </li>
+                        <li class="bodyLink_footer">
+                            <a href="/">Movies</a>
+                        </li>
+                        <li class="bodyLink_footer">
+                            <a href="/">Tv</a>
+                        </li>
+                        <li class="bodyLink_footer">
+                            <a href="/">Games</a>
+                        </li>
+                        <li class="bodyLink_footer">
+                            <a href="/">Videos</a>
+                        </li>
+                        <li class="bodyLink_footer">
+                            <a href="/">News</a>
+                        </li>
+                    </div>
+                    <div class="down_footer_links">
+                        <li class="headingLink_footer">Shop</li>
+                        <li class="bodyLink_footer">
+                            <a href="/">Shop DC</a>
+                        </li>
+                        <li class="bodyLink_footer">
+                            <a href="/">Shop DC Collectibles</a>
+                        </li>
+                    </div>
+                </ul>
+                <ul class="row_links_footer">
+                    <li class="headingLink_footer">Dc</li>
+                    <li class="bodyLink_footer">
+                        <a href="/">Terms Of Use</a>
+                    </li>
+                    <li class="bodyLink_footer">
+                        <a href="/">Privacy policy (New)</a>
+                    </li>
+                    <li class="bodyLink_footer">
+                        <a href="/">Ad Choices</a>
+                    </li>
+                    <li class="bodyLink_footer">
+                        <a href="/">Advertising</a>
+                    </li>
+                    <li class="bodyLink_footer">
+                        <a href="/">Jobs</a>
+                    </li>
+                    <li class="bodyLink_footer">
+                        <a href="/">Subscriptions</a>
+                    </li>
+                    <li class="bodyLink_footer">
+                        <a href="/">Talent Workshops</a>
+                    </li>
+                    <li class="bodyLink_footer">
+                        <a href="/">CPSC Certificates</a>
+                    </li>
+                    <li class="bodyLink_footer">
+                        <a href="/">Ratings</a>
+                    </li>
+                    <li class="bodyLink_footer">
+                        <a href="/">Shop Help</a>
+                    </li>
+                    <li class="bodyLink_footer">
+                        <a href="/">Contact Us</a>
+                    </li>
+                </ul>
+                <ul class="row_links_footer">
+                    <li class="headingLink_footer">Sites</li>
+                    <li class="bodyLink_footer">
+                        <a href="/">DC</a>
+                    </li>
+                    <li class="bodyLink_footer">
+                        <a href="/">MAD Magazine</a>
+                    </li>
+                    <li class="bodyLink_footer">
+                        <a href="/">DC Kids</a>
+                    </li>
+                    <li class="bodyLink_footer">
+                        <a href="/">DC Universe</a>
+                    </li>
+                    <li class="bodyLink_footer">
+                        <a href="/">DC Power Visa</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- Logo BG -->
+            <div class="logo_bg"></div>
+        </div>
     </div>
 </template>
