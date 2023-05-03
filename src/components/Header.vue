bd202624ece0b6125c546d7da3b3821ec9519d57
CarloBattista <carlitobatti@gmail.com>
2023-05-03 16:20:21 +0200

    creazione hamburger menu + aggiunta media query

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,26 @@
 <script>
 export default {
     name: "Header",
+    data() {
+        return {
+            menuVisible: false,
+            isOpen: false,
+        }
+    },
+    computed: {
+        isOpen() {
+            return this.menuVisible ? 'fa-solid fa-xmark' : 'fa-solid fa-bars';
+        }
+    },
+    methods: {
+        triggerMenuMobile() {
+            if (this.menuVisible == false) {
+                this.menuVisible = true;
+            } else {
+                this.menuVisible = false;
+            }
+        }
+    },
 }
 </script>
 
@@ -28,7 +48,7 @@ export default {
                 </li>
                 <li class="listItem">
                     <a class="linkPage" href="/games">Games</a>
-                    </li>
+                </li>
                 <li class="listItem">
                     <a class="linkPage" href="/collectibles">Collectibles</a>
                 </li>
@@ -45,6 +65,50 @@ export default {
                     <a class="linkPage" href="/shop">Shop</a>
                 </li>
             </ul>
+            <!-- Mobile -->
+            <div class="box_func_hamburger">
+                <!-- Button hamburger trigger -->
+                <button class="btn_trigger_hamburger" @click="triggerMenuMobile">
+                    <i class="" :class="isOpen"></i>
+                </button>
+                <!-- Hamburger menu -->
+                <div class="hamburger_menu" v-show="menuVisible">
+                    <ul class="list_nav_mobile">
+                        <li class="list_mobile">
+                            <a class="linkPage" href="/characters">Characters</a>
+                        </li>
+                        <li class="list_mobile mobile_page_active">
+                            <a class="linkPage" href="/comics">Comics</a>
+                        </li>
+                        <li class="list_mobile">
+                            <a class="linkPage" href="/movies">Movies</a>
+                        </li>
+                        <li class="list_mobile">
+                            <a class="linkPage" href="/tv">Tv</a>
+                        </li>
+                        <li class="list_mobile">
+                            <a class="linkPage" href="/games">Games</a>
+                        </li>
+                        <li class="list_mobile">
+                            <a class="linkPage" href="/collectibles">Collectibles</a>
+                        </li>
+                        <li class="list_mobile">
+                            <a class="linkPage" href="/videos">Videos</a>
+                        </li>
+                        <li class="list_mobile">
+                            <a class="linkPage" href="/fans">Fans</a>
+                        </li>
+                        <li class="list_mobile">
+                            <a class="linkPage" href="/news">News</a>
+                        </li>
+                        <li class="list_mobile">
+                            <a class="linkPage" href="/shop">Shop</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </div>
     </div>
 </template>
+
+<style></style>
