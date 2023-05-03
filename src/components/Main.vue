f613a5939643ea072e04308cac3f4b273953222c
CarloBattista <carlitobatti@gmail.com>
2023-05-03 15:02:23 +0200

    creato banner info

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -5,10 +5,51 @@ export default {
 </script>
 
 <template>
+    <!-- MainView -->
     <div class="mainView">
-        <div class="inner_main">
-            <div class="container__content">
-                <h2 class="content_place">Content goes here</h2>
+        <!-- Container content -->
+        <div class="container__content">
+            <div class="inner_content">
+                <h2 class="content_place">...Content goes here...</h2>
+            </div>
+        </div>
+        <!-- Container Banner -->
+        <div class="container__banner">
+            <div class="inner_content">
+                <!-- Container Card -->
+                <div class="row_container">
+                    <!-- Card -->
+                    <div class="row_card">
+                        <a href="/">
+                            <img class="imgText" src="/imgs/buy-comics-digital-comics.png" alt="">
+                            <span class="cardText">Digital Comics</span>
+                        </a>
+                    </div>
+                    <div class="row_card">
+                        <a href="/">
+                            <img class="imgText" src="/imgs/buy-comics-merchandise.png" alt="">
+                            <span class="cardText">Dc Merchandise</span>
+                        </a>
+                    </div>
+                    <div class="row_card">
+                        <a href="/">
+                            <img class="imgText" src="/imgs/buy-comics-subscriptions.png" alt="">
+                            <span class="cardText">Subscription</span>
+                        </a>
+                    </div>
+                    <div class="row_card">
+                        <a href="/">
+                            <img class="imgText" src="/imgs/buy-comics-shop-locator.png" alt="">
+                            <span class="cardText">Comic Shop Location</span>
+                        </a>
+                    </div>
+                    <div class="row_card">
+                        <a href="/">
+                            <img class="imgText" src="/imgs/buy-dc-power-visa.svg" alt="">
+                            <span class="cardText">Dc Power Visa</span>
+                        </a>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
