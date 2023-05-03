53a1b828398e80d17c10d716e160fb7cb1c80b49
CarloBattista <carlitobatti@gmail.com>
2023-05-03 14:44:56 +0200

    sezione content

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -5,5 +5,11 @@ export default {
 </script>
 
 <template>
-    <div class="mainView"></div>
+    <div class="mainView">
+        <div class="inner_main">
+            <div class="container__content">
+                <h2 class="content_place">Content goes here</h2>
+            </div>
+        </div>
+    </div>
 </template>
