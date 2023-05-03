7f3a52721ab7ef84de3b685e5435d20c60159de8
CarloBattista <carlitobatti@gmail.com>
2023-05-03 14:20:27 +0200

    stile header, parte logo

--- src/App.vue
+++ src/App.vue
@@ -15,4 +15,19 @@ export default {
 
 <style lang="scss">
 @use "./style/main.scss" as *;
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+  text-decoration: none;
+  list-style: none;
+  list-style-type: none;
+}
+
+body {
+  font-family: 'Source Sans Pro', sans-serif;
+  background: #fff;
+  color: #1C1C1C;
+}
+
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -7,41 +7,43 @@ export default {
 <template>
     <div>
         <div class="header">
-            <a href="/home" class="logoSite">
-                <img src="/imgs/dc-logo.png" alt="">
-            </a>
-            <ul class="nav">
-                <li class="listItem">
-                    <a href="/characters">Characters</a>
-                </li>
-                <li class="listItem">
-                    <a href="/comics">Comics</a>
-                </li>
-                <li class="listItem">
-                    <a href="/movies">Movies</a>
-                </li>
-                <li class="listItem">
-                    <a href="/tv">Tv</a>
-                </li>
-                <li class="listItem">
-                    <a href="/games">Games</a>
-                </li>
-                <li class="listItem">
-                    <a href="/collectibles">Collectibles</a>
-                </li>
-                <li class="listItem">
-                    <a href="/videos">Videos</a>
-                </li>
-                <li class="listItem">
-                    <a href="/fans">Fans</a>
-                </li>
-                <li class="listItem">
-                    <a href="/news">News</a>
-                </li>
-                <li class="listItem">
-                    <a href="/shop">Shop</a>
-                </li>
-            </ul>
+            <div class="inner_header">
+                <a href="/" class="logoSite">
+                    <img class="icon_logo" src="/imgs/dc-logo.png" alt="">
+                </a>
+                <ul class="nav">
+                    <li class="listItem">
+                        <a href="/characters">Characters</a>
+                    </li>
+                    <li class="listItem currentPage">
+                        <a href="/comics">Comics</a>
+                    </li>
+                    <li class="listItem">
+                        <a href="/movies">Movies</a>
+                    </li>
+                    <li class="listItem">
+                        <a href="/tv">Tv</a>
+                    </li>
+                    <li class="listItem">
+                        <a href="/games">Games</a>
+                    </li>
+                    <li class="listItem">
+                        <a href="/collectibles">Collectibles</a>
+                    </li>
+                    <li class="listItem">
+                        <a href="/videos">Videos</a>
+                    </li>
+                    <li class="listItem">
+                        <a href="/fans">Fans</a>
+                    </li>
+                    <li class="listItem">
+                        <a href="/news">News</a>
+                    </li>
+                    <li class="listItem">
+                        <a href="/shop">Shop</a>
+                    </li>
+                </ul>
+            </div>
         </div>
     </div>
 </template>
