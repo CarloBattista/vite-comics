9aa430096aba2363cb4afb046e885f1c26f2c89b
CarloBattista <carlitobatti@gmail.com>
2023-05-04 14:09:00 +0200

    ciclo card classico

--- src/components/MainComp.vue
+++ src/components/MainComp.vue
@@ -1,6 +1,84 @@
 <script>
 export default {
     name: "MainComp",
+    data() {
+        return {
+            series: [
+                {
+                    "thumb": "https://www.coverbrowser.com/image/action-comics/1-1.jpg",
+                    "price": "$19.99",
+                    "series": "Action Comics",
+                    "type": "comic book",
+                },
+                {
+                    "thumb": "https://www.panini.it/media/catalog/product/cache/a5b5dd3adfe0d321084804c738f29601/M/1/M1BLLA015ISBN_0.jpg",
+                    "price": "$3.99",
+                    "series": "American Vampire 1976",
+                    "type": "comic book",
+                },
+                {
+                    "thumb": "https://media.wired.com/photos/593384dad80dd005b42b2817/master/w_2560%2Cc_limit/Aquaman-16.jpg",
+                    "price": "$16.99",
+                    "series": "Aquaman",
+                    "type": "graphic novel",
+                },
+                {
+                    "thumb": "https://d29xot63vimef3.cloudfront.net/image/batgirl/1-1.jpg",
+                    "price": "$2.99",
+                    "series": "Batgirl",
+                    "type": "comic book",
+                },
+                {
+                    "thumb": "https://static.posters.cz/image/750/locandine-film-in-plexiglass-batman-prowl-comic-cover-i69653.jpg",
+                    "price": "$3.99",
+                    "series": "Batman",
+                    "type": "comic book",
+                },
+                {
+                    "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/5/50/Batman_Beyond_v.1_1.jpg",
+                    "price": "$2.99",
+                    "series": "Batman Beyond",
+                    "type": "comic book",
+                },
+                {
+                    "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/0/0d/Batman_Superman_Vol_1_1.jpg",
+                    "price": "$3.99",
+                    "series": "Batman/Superman",
+                    "type": "comic book",
+                },
+                {
+                    "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/c/cf/Batman_Superman_Annual_Vol_2_1.jpg",
+                    "price": "$4.99",
+                    "series": "Batman/Superman Annual",
+                    "type": "comic book",
+                },
+                {
+                    "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/5/54/Batman_The_Joker_War_Zone_Vol_1_1.jpg",
+                    "price": "$5.99",
+                    "series": "Batman: The Joker War Zone",
+                    "type": "comic book",
+                },
+                {
+                    "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/6/64/Batman_Three_Jokers_Collected.jpg",
+                    "price": "$6.99",
+                    "series": "Batman: Three Jokers",
+                    "type": "comic book",
+                },
+                {
+                    "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/f/f8/Batman_White_Knight_Presents_Harley_Quinn_Vol_1_1.jpg",
+                    "price": "$4.99",
+                    "series": "Batman: White Knight Presents: Harley Quinn",
+                    "type": "comic book",
+                },
+                {
+                    "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/c/c8/Catwoman_Vol_2_1.jpg",
+                    "price": "$16.99",
+                    "series": "Catwoman",
+                    "type": "graphic novel",
+                },
+            ]
+        }
+    }
 }
 </script>
 
@@ -9,7 +87,9 @@ export default {
     <div class="mainView">
         <!-- Container Hero -->
         <div class="container__hero">
-            <img class="hero_img" src="https://bleedingcool.com/wp-content/uploads/2020/06/DC-Comics-Spotify-Logo-1200x628.jpg" alt="hero image">
+            <img class="hero_img"
+                src="https://bleedingcool.com/wp-content/uploads/2020/06/DC-Comics-Spotify-Logo-1200x628.jpg"
+                alt="hero image">
         </div>
         <!-- Container content -->
         <div class="container__content">
@@ -18,123 +98,13 @@ export default {
                     <h2 class="heading_content">Current Series</h2>
                 </div>
                 <div class="container_series">
-                    <div class="cardSeries">
+                    <div class="cardSeries" v-for="(element, index) in series" :key="index">
                         <a href="/">
                             <div class="imgWrap">
-                                <img class="img_series" src="https://media.wired.com/photos/593384dad80dd005b42b2817/master/w_2560%2Cc_limit/Aquaman-16.jpg" alt="series image">
+                                <img class="img_series" :src="element.thumb" :alt="'image of ' + element.series">
                             </div>
                             <div class="infoWrap">
-                                <p class="title_series">Nome serie</p>
-                            </div>
-                        </a>
-                    </div>
-                    <div class="cardSeries">
-                        <a href="/">
-                            <div class="imgWrap">
-                                <img class="img_series" src="https://media.wired.com/photos/593384dad80dd005b42b2817/master/w_2560%2Cc_limit/Aquaman-16.jpg" alt="series image">
-                            </div>
-                            <div class="infoWrap">
-                                <p class="title_series">Nome serie</p>
-                            </div>
-                        </a>
-                    </div>
-                    <div class="cardSeries">
-                        <a href="/">
-                            <div class="imgWrap">
-                                <img class="img_series" src="https://media.wired.com/photos/593384dad80dd005b42b2817/master/w_2560%2Cc_limit/Aquaman-16.jpg" alt="series image">
-                            </div>
-                            <div class="infoWrap">
-                                <p class="title_series">Nome serie</p>
-                            </div>
-                        </a>
-                    </div>
-                    <div class="cardSeries">
-                        <a href="/">
-                            <div class="imgWrap">
-                                <img class="img_series" src="https://media.wired.com/photos/593384dad80dd005b42b2817/master/w_2560%2Cc_limit/Aquaman-16.jpg" alt="series image">
-                            </div>
-                            <div class="infoWrap">
-                                <p class="title_series">Nome serie</p>
-                            </div>
-                        </a>
-                    </div>
-                    <div class="cardSeries">
-                        <a href="/">
-                            <div class="imgWrap">
-                                <img class="img_series" src="https://media.wired.com/photos/593384dad80dd005b42b2817/master/w_2560%2Cc_limit/Aquaman-16.jpg" alt="series image">
-                            </div>
-                            <div class="infoWrap">
-                                <p class="title_series">Nome serie</p>
-                            </div>
-                        </a>
-                    </div>
-                    <div class="cardSeries">
-                        <a href="/">
-                            <div class="imgWrap">
-                                <img class="img_series" src="https://media.wired.com/photos/593384dad80dd005b42b2817/master/w_2560%2Cc_limit/Aquaman-16.jpg" alt="series image">
-                            </div>
-                            <div class="infoWrap">
-                                <p class="title_series">Nome serie</p>
-                            </div>
-                        </a>
-                    </div>
-                    <div class="cardSeries">
-                        <a href="/">
-                            <div class="imgWrap">
-                                <img class="img_series" src="https://media.wired.com/photos/593384dad80dd005b42b2817/master/w_2560%2Cc_limit/Aquaman-16.jpg" alt="series image">
-                            </div>
-                            <div class="infoWrap">
-                                <p class="title_series">Nome serie</p>
-                            </div>
-                        </a>
-                    </div>
-                    <div class="cardSeries">
-                        <a href="/">
-                            <div class="imgWrap">
-                                <img class="img_series" src="https://media.wired.com/photos/593384dad80dd005b42b2817/master/w_2560%2Cc_limit/Aquaman-16.jpg" alt="series image">
-                            </div>
-                            <div class="infoWrap">
-                                <p class="title_series">Nome serie</p>
-                            </div>
-                        </a>
-                    </div>
-                    <div class="cardSeries">
-                        <a href="/">
-                            <div class="imgWrap">
-                                <img class="img_series" src="https://media.wired.com/photos/593384dad80dd005b42b2817/master/w_2560%2Cc_limit/Aquaman-16.jpg" alt="series image">
-                            </div>
-                            <div class="infoWrap">
-                                <p class="title_series">Nome serie</p>
-                            </div>
-                        </a>
-                    </div>
-                    <div class="cardSeries">
-                        <a href="/">
-                            <div class="imgWrap">
-                                <img class="img_series" src="https://media.wired.com/photos/593384dad80dd005b42b2817/master/w_2560%2Cc_limit/Aquaman-16.jpg" alt="series image">
-                            </div>
-                            <div class="infoWrap">
-                                <p class="title_series">Nome serie</p>
-                            </div>
-                        </a>
-                    </div>
-                    <div class="cardSeries">
-                        <a href="/">
-                            <div class="imgWrap">
-                                <img class="img_series" src="https://media.wired.com/photos/593384dad80dd005b42b2817/master/w_2560%2Cc_limit/Aquaman-16.jpg" alt="series image">
-                            </div>
-                            <div class="infoWrap">
-                                <p class="title_series">Nome serie</p>
-                            </div>
-                        </a>
-                    </div>
-                    <div class="cardSeries">
-                        <a href="/">
-                            <div class="imgWrap">
-                                <img class="img_series" src="https://media.wired.com/photos/593384dad80dd005b42b2817/master/w_2560%2Cc_limit/Aquaman-16.jpg" alt="series image">
-                            </div>
-                            <div class="infoWrap">
-                                <p class="title_series">Nome serie</p>
+                                <p class="title_series">{{ element.series }}</p>
                             </div>
                         </a>
                     </div>
